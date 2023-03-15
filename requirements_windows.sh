b0ed96b3a1b24dc488302dd57f19545ce08ec67f
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-03-15 10:15:25 +0700

    verify tlmgr installation

--- requirements_windows.sh
+++ requirements_windows.sh
@@ -32,12 +32,14 @@ texlive_packages=(
 
 choco install texlive --version=2022.20221202 --params "'/InstallationPath:C:\texlive /collections:pictures,latex'"
 
-tlmgr="C:/texlive/2022/bin/win32/tlmgr.bat"
+# tlmgr="C:/texlive/2022/bin/win32/tlmgr.bat"
 
-# Update TeX Live package database
-$tlmgr update --self --all
+# # Update TeX Live package database
+# $tlmgr update --self --all
 
-# Install each package in the list
-for package in ${texlive_packages[@]}; do
-  $tlmgr install "$package"
-done
+# # Install each package in the list
+# for package in ${texlive_packages[@]}; do
+#   $tlmgr install "$package"
+# done
+
+export texlive_packages
